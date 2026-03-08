6b52aa4cf8ebd3872ed6a04c2c51aa8859a7f740
Rodhi Faisal Mufid <almufid.business@gmail.com>
2026-03-08 15:02:23 +0700

    New Reseller (Rifo)

--- data/resellers.ts
+++ data/resellers.ts
@@ -12,11 +12,23 @@ export const resellers = [
     name: "HANDRIO",
     phone: "6285187483025",
     email: "cs.hintech@gmail.com",
-    photo: "/dio.jpeg",
+    photo: "/reseller/dio.jpeg",
     sold: 39,
     rating: 5,
     location: "Indonesia",
     about:
       "Profesional Reseller Partner di PT. Heroic Inovasi Nusantara yang membantu bisnis memiliki website profesional, SEO, AI dan desain berkualitas.",
   },
+  {
+    slug: "rifo",
+    name: "RIFO MARGIASNYAH",
+    phone: "6285267757358",
+    email: "cs.hintech@gmail.com",
+    photo: "/reseller/rifo.png",
+    sold: 25,
+    rating: 5,
+    location: "Indonesia",
+    about:
+      "Profesional Reseller Partner di PT. Heroic Inovasi Nusantara yang membantu bisnis memiliki website profesional, SEO, AI dan desain berkualitas.",
+  }
 ]
